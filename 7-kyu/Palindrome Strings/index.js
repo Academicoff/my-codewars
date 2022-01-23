5e272788551e87e46e8256ee0e58a9f40015a73f
Academicoff <academic.official.inf@gmail.com>
2022-01-23 20:17:59 +0300

    add Best Practice

--- 7-kyu/Palindrome Strings/index.js
+++ 7-kyu/Palindrome Strings/index.js
@@ -4,7 +4,12 @@ function isPalindrome(line) {
 }
 
 //Best Practice
+// ### 1
 // function isPalindrome(line) {
 //     return (String(line) == String(line).split('').reverse().join('') )
 //
+// }
+// ### 2
+// function isPalindrome(line) {
+//     return (line.toString() == line.toString().split('').reverse().join(''));
 // }
