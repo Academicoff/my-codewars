8001b7e340802a897f1ae55961ef069c046f8528
Academicoff <academic.official.inf@gmail.com>
2022-01-26 02:01:25 +0300

    add Bset solution Char Code Calculation

--- 7-kyu/Char Code Calculation/index.test.js
+++ 7-kyu/Char Code Calculation/index.test.js
@@ -11,3 +11,30 @@ describe("Char Code Calculation", () => {
         assert.strictEqual(calc('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 96);
     })
 });
+
+//best solution
+//
+// ##1
+// function calc(x){
+//     let sum = n => [...n].reduce((a,b) => +a + +b);
+//     let total1 = x.replace(/./g, x => x.charCodeAt(0));
+//     let total2 = total1.replace(/7/g,'1');
+//     return sum(total1) - sum(total2);
+// }
+//
+// ##2
+//
+// const calc = x =>
+//     x.replace(/./g, val => val.charCodeAt()).replace(/[^7]/g, ``).length * 6;
+//
+// ##3
+// function calc(x){
+//     return x
+//         .split('')
+//         .map(c => c.charCodeAt(0))
+//         .join('')
+//         .split('')
+//         .map(Number)
+//         .filter(x => x === 7)
+//         .length * 6
+// }
