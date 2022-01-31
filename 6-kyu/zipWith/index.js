65c6a766582979bcc3cf5dc78810d78ee507eebd
Academicoff <academic.official.inf@gmail.com>
2022-01-31 22:40:51 +0300

    add zipwith

---
+++ 6-kyu/zipWith/index.js
@@ -0,0 +1,18 @@
+function zipWith(fn,a0,a1) {
+    arr = [];
+    let length = (a0.length > a1.length) ? a1.length : a0.length;
+    for (let i = 0; i < length; i++ ){
+        arr.push(fn(a0[i],a1[i]));
+    }
+    return arr;
+}
+
+//best solution
+//
+// // ##1
+// function zipWith(fn,a0,a1) {
+//     return Array.from({length: Math.min(a0.length, a1.length)}, (_, i) => fn(a0[i], a1[i]));
+// }
+//
+// ## 2
+// const zipWith = (f, a, b) => Array.from({length: Math.min(a.length, b.length)}, (_, i) => f(a[i], b[i]));
---
+++ 6-kyu/zipWith/index.test.js
@@ -0,0 +1,16 @@
+const {assert,config} = require("chai"); config.truncateThreshold = 0;
+
+describe("zipWith",()=>{
+    const plus = (a,b) => a+b ;
+    it("Example tests",()=>{
+        assert.deepEqual(zipWith(plus, [0,1,2,3,4,5], [6,5,4,3,2,1]), [6,6,6,6,6,6]);
+        assert.deepEqual(zipWith(plus, [0,1,2,3,4  ], [6,5,4,3,2,1]), [6,6,6,6,6  ]);
+        assert.deepEqual(zipWith(plus, [0,1,2,3,4,5], [6,5,4,3,2  ]), [6,6,6,6,6  ]);
+    });
+    it("Description tests",()=>{
+        assert.deepEqual(zipWith( Math.pow, [10,10,10,10], [0,1,2,3] ), [1,10,100,1000]);
+        assert.deepEqual(zipWith( Math.max, [1,4,7,1,4,7], [4,7,1,4,7,1] ), [4,7,7,4,7,7]);
+        assert.deepEqual(zipWith( function(a,b) { return a+b; }, [0,1,2,3], [0,1,2,3] ), [0,2,4,6]);
+        assert.deepEqual(zipWith( (a,b) => a+b, [0,1,2,3], [0,1,2,3] ), [0,2,4,6]);
+    });
+});
